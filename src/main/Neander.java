c75ac6ce0e149ab39f7bce7000f492dea91efef3
Desiree Santos <desisant@gmail.com>
2013-06-17 22:36:33 -0300

    Change file path name

--- src/main/Neander.java
+++ src/main/Neander.java
@@ -7,11 +7,7 @@ import control.StartProcessNeander;
  * Date: 6/16/13  - Time: 2:39 AM
  */
 public class Neander {
-
     public static void main(String[] args) {
-
-        new StartProcessNeander("enander.txt");
+        new StartProcessNeander("file/enander.txt");
     }
-
-
 }
